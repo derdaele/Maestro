c7559f4d20572bbc2ce3467f565982a91a38f753
Jérémy Derdaele <jeremy.derdaele@gmail.com>
2019-04-13 12:51:06 +0200

    Add retry-after logic in case of too many request

--- internal/spotify/constants.go
+++ internal/spotify/constants.go
@@ -4,4 +4,5 @@ const (
 	authTokenEndpoint    = "https://accounts.spotify.com/api/token"
 	baseEndpoint         = "https://api.spotify.com/v1"
 	maxRequestAlbumCount = 20
+	maxRetryCount        = 5
 )
--- internal/spotify/http.go
+++ internal/spotify/http.go
@@ -6,6 +6,8 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
+	"strconv"
+	"time"
 )
 
 // Request perform an HTTP request and parse the JSON body
@@ -14,19 +16,32 @@ func (c Client) Request(req *http.Request, result interface{}) error {
 	req.Header.Set("Accept", "application/json")
 	req.Header.Set("Content-Type", "application/json")
 
-	res, err := http.DefaultClient.Do(req)
+	for i := 0; i < maxRetryCount; i++ {
+		res, err := http.DefaultClient.Do(req)
 
-	if err != nil {
-		return err
+		if err != nil {
+			return err
+		}
+
+		switch code := res.StatusCode; {
+		case code == 429: // Too many request
+			retryAfterSec, _ := strconv.Atoi(res.Header.Get("Retry-After"))
+			time.Sleep(time.Second * time.Duration(retryAfterSec))
+
+		case code >= 200 && code < 300: // Success
+			payload, err := ioutil.ReadAll(res.Body)
+
+			if err != nil {
+				return err
+			}
+
+			return json.Unmarshal(payload, result)
+		default:
+			return fmt.Errorf("Unexpected status code %d", code)
+		}
 	}
 
-	payload, err := ioutil.ReadAll(res.Body)
-
-	if err != nil {
-		return err
-	}
-
-	return json.Unmarshal(payload, result)
+	return nil
 }
 
 // Get perform a GET http request on the URL and extract the json body result
